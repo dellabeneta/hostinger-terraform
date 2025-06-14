718e97720a49e034eec4512786a6eeaecd7d87ba
dellabeneta <m.dellabeneta@gmail.com>
2025-06-14 15:04:51 -0300

    criação dinâmica

--- variables.tf
+++ variables.tf
@@ -1,35 +1,40 @@
 
 variable "api_token" {
-  type = string
+  type        = string
   description = "Token para a API da Hostinger"
 }
 
 variable "password" {
-  type = string
+  type        = string
   description = "Senha do usuário ROOT do Linux"
 }
 
 variable "hostname" {
-  type = string
+  type        = string
   description = "Hostname para a VPS"
 }
 
 variable "region_id" {
-  type = string
+  type        = string
   description = "Região do Datacenter"
 }
 
 variable "template_id" {
-  type = string
+  type        = string
   description = "Sistema Operacional da VPS"
 }
 
 variable "plan" {
-  type = string
+  type        = string
   description = "Plano para a VPS"
 }
 
 variable "ssh_key_ids" {
-  type = list(number)
+  type        = list(number)
   description = "ID da chave cadastrada na Hostinger"
+}
+
+variable "domain" {
+  type        = string
+  description = "Nome de domínio FQDN obritário"
 }
--- vps.tf
+++ vps.tf
@@ -1,9 +1,24 @@
 
-resource "hostinger_vps" "vps" {
+# resource "hostinger_vps" "vps" {
+#   plan           = var.plan
+#   data_center_id = var.region_id
+#   template_id    = var.template_id
+#   hostname       = var.hostname
+#   password       = var.password
+#   ssh_key_ids    = var.ssh_key_ids
+
+#   post_install_script_id = hostinger_vps_post_install_script.nginx.id
+
+# }
+
+// exemplo para criação de 5 vps iguais
+
+resource "hostinger_vps" "vps5" {
+  count          = 5
   plan           = var.plan
   data_center_id = var.region_id
   template_id    = var.template_id
-  hostname       = var.hostname
+  hostname       = "server-${count.index + 1}.${var.domain}"
   password       = var.password
   ssh_key_ids    = var.ssh_key_ids
 
